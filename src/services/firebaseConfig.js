50e2df6075c4599c5022e7bd60adf84311352492
GABRIEL DE SOUZA FURTADO <121953504+gabrielsousaf@users.noreply.github.com>
2023-07-13 13:38:43 -0300

    Config Firebase

---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIIJf63R-u2yuXmIRgXClW2dUPdGFRfSc",
+  authDomain: "react-auth-2017a.firebaseapp.com",
+  projectId: "react-auth-2017a",
+  storageBucket: "react-auth-2017a.appspot.com",
+  messagingSenderId: "883134463693",
+  appId: "1:883134463693:web:b2799a28ebe7d459427384"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
