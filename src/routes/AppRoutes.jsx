cba6946a0cb44ced52425887b55e1999713e24f5
GABRIEL DE SOUZA FURTADO <121953504+gabrielsousaf@users.noreply.github.com>
2023-07-17 12:59:25 -0300

    Udate AuthContext

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,16 +1,42 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useState, useEffect } from "react";
+import { useAuthentication } from "../hooks/UseAuthentication";
 
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
 
+import { AuthProvider } from "../context/AuthContext";
+import Dashboard from "../pages/Dashboard/Dashboard";
+
 const AppRoutes = ()  => {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider value={ user }>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
