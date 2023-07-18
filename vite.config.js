b08b2ec5ff02f92c87dcd18488633a1d6ec60be4
GABRIEL DE SOUZA FURTADO <121953504+gabrielsousaf@users.noreply.github.com>
2023-07-18 20:58:30 -0300

    config deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  build: {
+    target: 'production'
+  },
   plugins: [react()],
   base: "/Login-Form-React",
 });
